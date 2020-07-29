940c0272670cf3740e499edf7c548a1079a22496
Nathan Tutchton <ntutchton@Nathans-MacBook-Pro.local>
2020-07-29 11:17:41 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { action } from './actions/action';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStateToProps = state => ({
+  ...state
+ })
+
+const mapDispatchToProps = dispatch => ({
+  d_action: () => dispatch(action())
+})
+
+class App extends React.Component {
+
+  reduxAction = (event) => {
+    this.props.d_action();
+   }
+
+  render(){
+    return (
+      <div className="app_global">
+          <pre>
+            {
+              JSON.stringify(this.props)
+            }
+          </pre>
+          <button onClick={this.reduxAction}>Test redux action</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
